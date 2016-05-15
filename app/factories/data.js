4820e96a660870fce9260f9cdcca137f8d146e6c
Mateusz Wilk <matewilk@gmail.com>
2016-05-16 00:22:25 +0100

    Removed debugger from factory

--- app/factories/data.js
+++ app/factories/data.js
@@ -2,7 +2,6 @@ export default function ($resource)
 {
     return {
         query: function(pollingUrl, callback){
-            debugger;
             return $resource(pollingUrl, {}, {
                 query: { method: 'GET', params: {}, headers: {'Access-Control-Allow-Origin': '*'}}
             }).query(callback);
